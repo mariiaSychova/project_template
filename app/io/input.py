aa003e16e4f6c916a676e5bcaffbbcef67000b7b
Mariia Sychova <mariiasychova3@gmail.com>
2024-03-18 20:32:10 +0200

    Added empty functions in input.py and output.py

---
+++ app/io/input.py
@@ -0,0 +1,12 @@
+
+
+def input_text_from_console():
+    pass
+
+
+def read_from_file_using_built_in_python_capabilities():
+    pass
+
+
+def read_from_file_using_pandas():
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,12 @@
+
+
+def output_text_to_console():
+    pass
+
+
+def write_to_file_using_built_in_python_capabilities():
+    pass
+
+
+def write_to_file_using_pandas():
+    pass
